d141b99d6333a99297fffed1d79ef58275fc9ebe
kota-aso <kotaaso1212@gmail.com>
2022-12-15 16:21:55 +0900

    コントローラーの編集

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,20 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    Question.create(quesion_params)
+    redirect_to action: :index
+  end
+
+  private
   
+  def quesion_params
+    params.require(:question).permit(:title, :content, :name)
+  end
 end
